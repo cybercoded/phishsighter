67f672254d636df2de2a3eaa98cba3d3a3df24f3
kunley247 <cafeat9ja@gmail.com>
2024-08-28 04:43:36 +0100

    update

--- static/function.js
+++ static/function.js
@@ -5,6 +5,28 @@ $(document).ready(function() {
         return urlParams.get(param);
     }
 
+    // Function to validate a URL and add http:// if necessary
+    function formatAndValidateUrl(urlString) {
+        // Check if the URL starts with http://, https://, or another protocol
+        if (!/^https?:\/\//i.test(urlString)) {
+            urlString = 'http://' + urlString;
+        }
+        try {
+            new URL(urlString);  // This will throw if the URL is invalid
+            return urlString;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    // Function to update the URL parameters without reloading the page
+    function updateUrlParams(url) {
+        const params = new URLSearchParams(window.location.search);
+        params.set('url', url);
+        const newUrl = `${window.location.pathname}?${params.toString()}`;
+        history.replaceState({}, '', newUrl);
+    }
+
     // Define the feature labels
     const featureLabels = [
         'Using IP', 'Long URL', 'Short URL', 'Symbol @', 'Redirecting //', 'Prefix/Suffix',
@@ -19,20 +41,35 @@ $(document).ready(function() {
     // Automatically submit form if URL is provided in the query params
     const urlParam = getQueryParam('url');
     if (urlParam) {
-        $('#url').val(urlParam); // Set the URL input value
-        // Delay the form submission slightly to ensure the DOM is ready
-        setTimeout(function() {
-            $('#urlForm').trigger('submit'); // Automatically submit the form
-        }, 100); // Adjust the delay as necessary
+        const formattedUrl = formatAndValidateUrl(urlParam);
+        if (formattedUrl) {
+            $('#url').val(formattedUrl); // Set the URL input value
+            // Delay the form submission slightly to ensure the DOM is ready
+            setTimeout(function() {
+                $('#urlForm').trigger('submit'); // Automatically submit the form
+            }, 100); // Adjust the delay as necessary
+        } else {
+            alert('Invalid URL provided.');
+        }
     }
 
     // Handle form submission
     $('#urlForm').on('submit', function(event) {
         event.preventDefault();  // Prevent default form submission
+
+        let url = $('#url').val();
+
+        url = formatAndValidateUrl(url);
+        if (!url) {
+            alert('Please enter a valid URL.');
+            return;
+        }
+
+        // Update the URL parameters
+        updateUrlParams(url);
+
         $('#results').collapse('hide');  // Hide previous results
         $('#loadingSpinner').show();  // Show the loading spinner
-
-        const url = $('#url').val();
 
         $.ajax({
             url: '/check_url',  // Adjust the URL if your Flask endpoint is different
@@ -77,6 +114,7 @@ $(document).ready(function() {
 
                 $('#results').collapse('show');  // Show the results section
             },
+            
             error: function(xhr, status, error) {
                 $('#loadingSpinner').hide();  // Hide the loading spinner
                 alert('An error occurred: ' + error);
@@ -85,12 +123,6 @@ $(document).ready(function() {
     });
 
     // Handle the toggle icon change on collapse show/hide
-    $('#featureExplanation').on('shown.bs.collapse', function () {
-        $(this).find('.toggle-icon').removeClass('fa-chevron-right').addClass('fa-chevron-down');
-    }).on('hidden.bs.collapse', function () {
-        $(this).find('.toggle-icon').removeClass('fa-chevron-down').addClass('fa-chevron-right');
-    });
-
     $('#extractedFeatures').on('shown.bs.collapse', function () {
         $(this).find('.toggle-icon').removeClass('fa-chevron-right').addClass('fa-chevron-down');
     }).on('hidden.bs.collapse', function () {
